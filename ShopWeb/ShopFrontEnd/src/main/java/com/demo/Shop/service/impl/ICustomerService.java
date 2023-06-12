333116ebc4c18269f0a77831c0237e8a2754b9ba
HUY DINH <dvanhuy11@gmail.com>
2023-06-12 23:48:09 +0700

    Huy Commit2

---
+++ ShopWeb/ShopFrontEnd/src/main/java/com/demo/Shop/service/impl/ICustomerService.java
@@ -0,0 +1,24 @@
+package com.demo.Shop.service.impl;
+
+import java.util.List;
+
+import com.demo.Shop.common.entity.AuthenticationType;
+//import com.demo.Shop.common.entity.Country;
+import com.Shop.common.entity.Customer;
+import com.demo.Shop.common.exception.CustomerNotFoundException;
+
+public interface ICustomerService {
+
+//    public List<Country> listAllCountries();
+    public boolean isEmailUnique(String email);
+    public void registerCustomer(Customer customer);
+    public boolean verify(String verificationCode);
+    public Customer getCustomerByEmail(String email);
+    public void updateAuthenticationType(Customer customer, AuthenticationType type);
+    public void addNewCustomerUponOAuthLogin(String name, String email, String countryCode,
+                                             AuthenticationType authenticationType);
+    public void update(Customer customerInForm);
+    public String updateResetPasswordToken(String email) throws CustomerNotFoundException;
+    public Customer getByResetPasswordToken(String token);
+    public void updatePassword(String token, String newPassword) throws CustomerNotFoundException;
+}
